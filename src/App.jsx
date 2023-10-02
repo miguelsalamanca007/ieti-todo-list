db582e6ddfd201bf9579b58fa4697b7f5d6466e6
miguelsalamanca007 <miguelangel62009@hotmail.com>
2023-10-02 14:07:09 -0500

    Added localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import TaskList from './components/TaskList';
 
 function App() {
-  const tasks = [
-    { name: 'Hacer front proyecto', state: 'Pendiente' },
-    { name: 'Hacer taller ieti', state: 'Completada' },
-    { name: 'Hacer BPM proyecto', state: 'Pendiente' },
-  ];
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  useEffect(() => {
+    const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    setTasks(savedTasks);
+  }, []);
+
+  const completeTask = (taskName) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.name === taskName) {
+        return { ...task, state: 'Completada' };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+  };
+
+  const addTask = () => {
+    if (newTask.trim() === '') {
+      return;
+    }
+    const taskToAdd = { name: newTask, state: 'Pendiente' };
+    const updatedTasks = [...tasks, taskToAdd];
+    setTasks(updatedTasks);
+    setNewTask('');
+    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+  };
 
   return (
     <div className="App">
       <Header />
-      <TaskList tasks={tasks} />
+      <div>
+        <input
+          type="text"
+          placeholder="Nueva tarea"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={addTask}>Agregar Tarea</button>
+      </div>
+      <TaskList tasks={tasks} completeTask={completeTask} />
     </div>
   );
 }
